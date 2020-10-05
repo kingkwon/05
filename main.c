0464bdf0dcbb432b700edf0cfac21e57676451f7
kingkwon <55428422+kingkwon@users.noreply.github.com>
2020-10-05 22:22:04 +0900

    실습 01

--- main.c
+++ main.c
@@ -4,5 +4,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	int a;
+	
+	printf("input a integer : ");
+	scanf("%d", &a);
+	
+	if(a>0)
+	{
+		printf("양수입니다.\n");
+	}
+	else if(a<0)
+	{
+		printf("음수입니다.\n");
+	}
+	else
+	{
+		printf("0입니다.\n");
+	}
+	
 	return 0;
 }
